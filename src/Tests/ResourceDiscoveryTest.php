761e822123a0657957dde793a24f47cc169d1dad
Ted Bowman <ted.git@tedbow.com>
2016-07-06 15:42:28 -0400

    Issue #2761073: Expand test coverage for resource to include user entity and a config entity

--- src/Tests/ResourceDiscoveryTest.php
+++ src/Tests/ResourceDiscoveryTest.php
@@ -27,14 +27,21 @@ class ResourceDiscoveryTest extends RESTTestBase {
    *
    * @var \GuzzleHttp\Cookie\CookieJar
    */
-  protected $guzzle_cookies;
+  protected $guzzleCookies;
 
   /**
    * Modules to enable.
    *
    * @var array
    */
-  public static $modules = ['system', 'node', 'serialization', 'rest', 'waterwheel'];
+  public static $modules = [
+    'system',
+    'node',
+    'taxonomy',
+    'serialization',
+    'rest',
+    'waterwheel',
+  ];
 
   /**
    * {@inheritdoc}
@@ -43,48 +50,68 @@ class ResourceDiscoveryTest extends RESTTestBase {
     parent::setUp();
     $this->defaultFormat = 'json';
     $this->defaultMimeType = 'application/json';
-    $this->guzzle_cookies = new CookieJar();
+    $this->guzzleCookies = new CookieJar();
     $encoders = [new JsonEncoder()];
     $this->serializer = new Serializer([], $encoders);
 
     $this->drupalCreateContentType(['type' => 'page', 'name' => 'Page']);
+
   }
 
   /**
    * Tests discovering resources.
    */
   public function testResourceDiscovery() {
+
     $user = $this->drupalCreateUser(
       [
         'view site configuration',
         'restful get entity_types_list_resource',
         'restful get bundle_type_resource',
+        'restful get entity_type_resource',
       ]
     );
     $this->drupalLogin($user);
+    $config = $this->config('rest.settings');
+    $this->verbose(print_r($config->getRawData(), 1));
+
+    $this->enableService('entity:user', 'GET', 'cookie');
+    $this->enableService('entity:taxonomy_vocabulary', 'GET', 'cookie');
 
     $result = $this->loginRequest($user->getAccountName(), $user->pass_raw);
     $this->assertEqual(200, $result->getStatusCode());
 
-
-    $url = Url::fromRoute('rest.entity_types_list_resource.GET.json')->setRouteParameter('_format', $this->defaultFormat);
-    $response = $this->httpRequest($url, 'GET');
-    $this->assertResponse(200);
-    $data = Json::decode($response);
-    $this->assertEqual($data, $this->getExpectedResources(), 'Resource list correct');
+    $url = Url::fromRoute('rest.entity_types_list_resource.GET.json')->setRouteParameter('_format', 'json');
+    $this->assertHttpResponse($url, 'GET', 200, $this->getExpectedResources(), 'Resource list correct');
 
     $url = Url::fromRoute('rest.bundle_type_resource.GET.json');
     $url->setRouteParameters(
       [
-        '_format' => $this->defaultFormat,
+        '_format' => 'json',
         'entity_type' => 'node',
         'bundle' => 'page',
       ]
     );
-    $response = $this->httpRequest($url, 'GET');
-    $this->assertResponse(200);
-    $data = Json::decode($response);
-    $this->assertEqual($data, $this->getExpectedBundle('node', 'page'), 'Page bundle information correct.');
+    $this->assertHttpResponse($url, 'GET', 200, $this->getExpectedBundle('node', 'page'), 'Page bundle information correct.');
+
+    $url = Url::fromRoute('rest.bundle_type_resource.GET.json');
+    $url->setRouteParameters(
+      [
+        '_format' => 'json',
+        'entity_type' => 'user',
+        'bundle' => 'user',
+      ]
+    );
+    $this->assertHttpResponse($url, 'GET', 200, $this->getExpectedBundle('user', 'user'), 'User bundle information correct.');
+
+    $url = Url::fromRoute('rest.entity_type_resource.GET.json');
+    $url->setRouteParameters(
+      [
+        '_format' => 'json',
+        'entity_type' => 'taxonomy_vocabulary',
+      ]
+    );
+    $this->assertHttpResponse($url, 'GET', 200, $this->getExpectedEntityType('taxonomy_vocabulary'), 'Vocabulary information correct.');
   }
 
   /**
@@ -119,7 +146,7 @@ class ResourceDiscoveryTest extends RESTTestBase {
         'Accept' => "application/$format",
       ],
       'http_errors' => FALSE,
-      'guzzle_cookies' => $this->guzzle_cookies,
+      'guzzle_cookies' => $this->guzzleCookies,
     ]);
     return $result;
   }
@@ -146,6 +173,26 @@ class ResourceDiscoveryTest extends RESTTestBase {
           'bundles' => ['page', 'resttest'],
           'more' => '/entity/types/node/{bundle}',
         ],
+      'user' =>
+        [
+          'label' => 'User',
+          'type' => 'content',
+          'methods' =>
+            [
+              'GET' => '/user/{user}',
+            ],
+          'more' => 'entity/types/user/user',
+        ],
+      'taxonomy_vocabulary' =>
+        array(
+          'label' => 'Taxonomy vocabulary',
+          'type' => 'config',
+          'methods' =>
+            array(
+              'GET' => '/entity/taxonomy_vocabulary/{taxonomy_vocabulary}',
+            ),
+          'more' => 'entity/types/taxonomy_vocabulary',
+        ),
     ];
     return $expected_resources;
   }
@@ -160,6 +207,7 @@ class ResourceDiscoveryTest extends RESTTestBase {
    *
    * @return array
    *   The array of expected results.
+   *
    * @throws \Exception
    *   If entity_type_id and bundle name are not supported.
    */
@@ -442,10 +490,350 @@ class ResourceDiscoveryTest extends RESTTestBase {
             ),
         ),
     );
+    $expected['user']['user'] = array(
+      'label' => 'User',
+      'fields' =>
+        array(
+          'uid' =>
+            array(
+              'label' => 'User ID',
+              'type' => 'integer',
+              'data_type' => 'list',
+              'required' => FALSE,
+              'readonly' => TRUE,
+              'cardinality' => 1,
+              'settings' =>
+                array(
+                  'unsigned' => TRUE,
+                  'size' => 'normal',
+                  'min' => '',
+                  'max' => '',
+                  'prefix' => '',
+                  'suffix' => '',
+                ),
+              'is_reference' => FALSE,
+            ),
+          'uuid' =>
+            array(
+              'label' => 'UUID',
+              'type' => 'uuid',
+              'data_type' => 'list',
+              'required' => FALSE,
+              'readonly' => TRUE,
+              'cardinality' => 1,
+              'settings' =>
+                array(
+                  'max_length' => 128,
+                  'is_ascii' => TRUE,
+                  'case_sensitive' => FALSE,
+                ),
+              'is_reference' => FALSE,
+            ),
+          'langcode' =>
+            array(
+              'label' => 'Language code',
+              'type' => 'language',
+              'data_type' => 'list',
+              'required' => FALSE,
+              'readonly' => FALSE,
+              'cardinality' => 1,
+              'settings' =>
+                array(),
+              'is_reference' => FALSE,
+            ),
+          'preferred_langcode' =>
+            array(
+              'label' => 'Preferred language code',
+              'type' => 'language',
+              'data_type' => 'list',
+              'required' => FALSE,
+              'readonly' => FALSE,
+              'cardinality' => 1,
+              'settings' =>
+                array(),
+              'is_reference' => FALSE,
+            ),
+          'preferred_admin_langcode' =>
+            array(
+              'label' => 'Preferred admin language code',
+              'type' => 'language',
+              'data_type' => 'list',
+              'required' => FALSE,
+              'readonly' => FALSE,
+              'cardinality' => 1,
+              'settings' =>
+                array(),
+              'is_reference' => FALSE,
+            ),
+          'name' =>
+            array(
+              'label' => 'Name',
+              'type' => 'string',
+              'data_type' => 'list',
+              'required' => TRUE,
+              'readonly' => FALSE,
+              'cardinality' => 1,
+              'settings' =>
+                array(
+                  'max_length' => 255,
+                  'is_ascii' => FALSE,
+                  'case_sensitive' => FALSE,
+                ),
+              'is_reference' => FALSE,
+            ),
+          'pass' =>
+            array(
+              'label' => 'Password',
+              'type' => 'password',
+              'data_type' => 'list',
+              'required' => FALSE,
+              'readonly' => FALSE,
+              'cardinality' => 1,
+              'settings' =>
+                array(
+                  'max_length' => 255,
+                  'is_ascii' => FALSE,
+                  'case_sensitive' => FALSE,
+                ),
+              'is_reference' => FALSE,
+            ),
+          'mail' =>
+            array(
+              'label' => 'Email',
+              'type' => 'email',
+              'data_type' => 'list',
+              'required' => FALSE,
+              'readonly' => FALSE,
+              'cardinality' => 1,
+              'settings' =>
+                array(),
+              'is_reference' => FALSE,
+            ),
+          'timezone' =>
+            array(
+              'label' => 'Timezone',
+              'type' => 'string',
+              'data_type' => 'list',
+              'required' => FALSE,
+              'readonly' => FALSE,
+              'cardinality' => 1,
+              'settings' =>
+                array(
+                  'max_length' => 32,
+                  'is_ascii' => FALSE,
+                  'case_sensitive' => FALSE,
+                ),
+              'is_reference' => FALSE,
+            ),
+          'status' =>
+            array(
+              'label' => 'User status',
+              'type' => 'boolean',
+              'data_type' => 'list',
+              'required' => FALSE,
+              'readonly' => FALSE,
+              'cardinality' => 1,
+              'settings' =>
+                array(
+                  'on_label' => 'On',
+                  'off_label' => 'Off',
+                ),
+              'is_reference' => FALSE,
+            ),
+          'created' =>
+            array(
+              'label' => 'Created',
+              'type' => 'created',
+              'data_type' => 'list',
+              'required' => FALSE,
+              'readonly' => FALSE,
+              'cardinality' => 1,
+              'settings' =>
+                array(),
+              'is_reference' => FALSE,
+            ),
+          'changed' =>
+            array(
+              'label' => 'Changed',
+              'type' => 'changed',
+              'data_type' => 'list',
+              'required' => FALSE,
+              'readonly' => FALSE,
+              'cardinality' => 1,
+              'settings' =>
+                array(),
+              'is_reference' => FALSE,
+            ),
+          'access' =>
+            array(
+              'label' => 'Last access',
+              'type' => 'timestamp',
+              'data_type' => 'list',
+              'required' => FALSE,
+              'readonly' => FALSE,
+              'cardinality' => 1,
+              'settings' =>
+                array(),
+              'is_reference' => FALSE,
+            ),
+          'login' =>
+            array(
+              'label' => 'Last login',
+              'type' => 'timestamp',
+              'data_type' => 'list',
+              'required' => FALSE,
+              'readonly' => FALSE,
+              'cardinality' => 1,
+              'settings' =>
+                array(),
+              'is_reference' => FALSE,
+            ),
+          'init' =>
+            array(
+              'label' => 'Initial email',
+              'type' => 'email',
+              'data_type' => 'list',
+              'required' => FALSE,
+              'readonly' => FALSE,
+              'cardinality' => 1,
+              'settings' =>
+                array(),
+              'is_reference' => FALSE,
+            ),
+          'roles' =>
+            array(
+              'label' => 'Roles',
+              'type' => 'entity_reference',
+              'data_type' => 'list',
+              'required' => FALSE,
+              'readonly' => FALSE,
+              'cardinality' => -1,
+              'settings' =>
+                array(
+                  'target_type' => 'user_role',
+                  'handler' => 'default',
+                  'handler_settings' =>
+                    array(),
+                ),
+              'is_reference' => TRUE,
+            ),
+          'default_langcode' =>
+            array(
+              'label' => 'Default translation',
+              'type' => 'boolean',
+              'data_type' => 'list',
+              'required' => FALSE,
+              'readonly' => FALSE,
+              'cardinality' => 1,
+              'settings' =>
+                array(
+                  'on_label' => 'On',
+                  'off_label' => 'Off',
+                ),
+              'is_reference' => FALSE,
+            ),
+        ),
+    );
     if (isset($expected[$entity_type_id][$bundle_name])) {
       return $expected[$entity_type_id][$bundle_name];
     }
     throw new \Exception("Unknown resource: $entity_type_id:$bundle_name");
+  }
+
+  /**
+   * Enables the REST service interface for a specific entity type.
+   *
+   * @todo Remove this override if this is fixed in
+   *   https://www.drupal.org/node/2761087
+   *   Current core version disables all other services.
+   *
+   * @param string|FALSE $resource_type
+   *   The resource type that should get REST API enabled or FALSE to disable
+   *   all resource types.
+   * @param string $method
+   *   The HTTP method to enable, e.g. GET, POST etc.
+   * @param string|array $format
+   *   (Optional) The serialization format, e.g. hal_json, or a list of formats.
+   * @param array $auth
+   *   (Optional) The list of valid authentication methods.
+   */
+  protected function enableService($resource_type, $method = 'GET', $format = NULL, $auth = NULL) {
+    // Enable REST API for this entity type.
+    $config = $this->config('rest.settings');
+    $resources = [];
+
+    if ($resource_type) {
+      $resources = $config->get('resources');
+      $resources[$resource_type] = [];
+      if (is_array($format)) {
+        $resources[$resource_type][$method]['supported_formats'] = $format;
+      }
+      else {
+        if ($format == NULL) {
+          $format = 'json';
+        }
+        $resources[$resource_type][$method]['supported_formats'][] = $format;
+      }
+
+      if ($auth == NULL) {
+        $auth = $this->defaultAuth;
+      }
+      $resources[$resource_type][$method]['supported_auth'] = $auth;
+    }
+    $config->set('resources', $resources);
+    $config->save();
+    $this->rebuildCache();
+  }
+
+  /**
+   * Asserts that HTTP response has expected response code and response body.
+   *
+   * @param \Drupal\Core\Url $url
+   *   The Url object.
+   * @param string $method
+   *   The HTTP method.
+   * @param int $status_code
+   *   The status code.
+   * @param array $expected_result
+   *   The expected result, un-encoded.
+   * @param string $message
+   *   The message to display if body is not expected result.
+   */
+  protected function assertHttpResponse(Url $url, $method, $status_code, $expected_result, $message = '') {
+    $response = $this->httpRequest($url, $method);
+    $this->assertResponse($status_code);
+    $data = Json::decode($response);
+    $this->assertEqual($data, $expected_result, $message);
+  }
+
+  /**
+   * Gets the expected results for an entity type.
+   *
+   * @param string $entity_type_id
+   *   The entity type id.
+   *
+   * @return array
+   *   Expected information for entity type.
+   *
+   * @throws \Exception
+   *   If unknown resource.
+   */
+  protected function getExpectedEntityType($entity_type_id) {
+    $entity_types = [
+      'taxonomy_vocabulary' => array(
+        'label' => 'Taxonomy vocabulary',
+        'type' => 'config',
+        'methods' =>
+          array(
+            'GET' => '/entity/taxonomy_vocabulary/{taxonomy_vocabulary}',
+          ),
+      ),
+    ];
+
+    if (isset($entity_types[$entity_type_id])) {
+      return $entity_types[$entity_type_id];
+    }
+    throw new \Exception("Unknown resource: $entity_type_id");
   }
 
 }
