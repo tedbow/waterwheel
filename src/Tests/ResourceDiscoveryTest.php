a1624b892533dcb49708f6911a8158857a0fd5b1
Ted Bowman <ted.git@tedbow.com>
2017-02-13 11:48:27 -0500

    Added OpenAPIResourceTest.php

--- src/Tests/ResourceDiscoveryTest.php
+++ src/Tests/ResourceDiscoveryTest.php
@@ -92,43 +92,6 @@ class ResourceDiscoveryTest extends WaterwheelTestBase {
   }
 
   /**
-   * Executes a login HTTP request.
-   *
-   * @param string $name
-   *   The username.
-   * @param string $pass
-   *   The user password.
-   * @param string $format
-   *   The format to use to make the request.
-   *
-   * @return \Psr\Http\Message\ResponseInterface The HTTP response.
-   *   The HTTP response.
-   */
-  protected function loginRequest($name, $pass, $format = 'json') {
-    $user_login_url = Url::fromRoute('user.login.http')
-      ->setRouteParameter('_format', $format)
-      ->setAbsolute();
-
-    $request_body = [];
-    if (isset($name)) {
-      $request_body['name'] = $name;
-    }
-    if (isset($pass)) {
-      $request_body['pass'] = $pass;
-    }
-
-    $result = \Drupal::httpClient()->post($user_login_url->toString(), [
-      'body' => $this->serializer->encode($request_body, $format),
-      'headers' => [
-        'Accept' => "application/$format",
-      ],
-      'http_errors' => FALSE,
-      'guzzle_cookies' => $this->guzzleCookies,
-    ]);
-    return $result;
-  }
-
-  /**
    * Returns expected resources list.
    *
    * @return array
@@ -715,29 +678,6 @@ class ResourceDiscoveryTest extends WaterwheelTestBase {
       return $expected[$entity_type_id][$bundle_name];
     }
     throw new \Exception("Unknown resource: $entity_type_id:$bundle_name");
-  }
-
-  /**
-   * Asserts that HTTP response has expected response code and response body.
-   *
-   * @param \Drupal\Core\Url $url
-   *   The Url object.
-   * @param string $method
-   *   The HTTP method.
-   * @param int $status_code
-   *   The status code.
-   * @param array $expected_result
-   *   The expected result, un-encoded.
-   * @param string $message
-   *   The message to display if body is not expected result.
-   */
-  protected function assertHttpResponse(Url $url, $method, $status_code, $expected_result, $message = '') {
-    $response = $this->httpRequest($url, $method);
-    $this->assertResponse($status_code);
-    if ($expected_result !== NULL) {
-      $data = Json::decode($response);
-      $this->assertEqual($data, $expected_result, $message);
-    }
   }
 
   /**
